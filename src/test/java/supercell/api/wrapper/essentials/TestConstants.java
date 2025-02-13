eda226e2377b6e4f453743143c9ea2eef0adc07f
mlieshoff <michael.lieshoff@gmail.com>
2025-02-13 16:37:59 +0100

    Remove checker related code

--- src/test/java/supercell/api/wrapper/essentials/TestConstants.java
+++ src/test/java/supercell/api/wrapper/essentials/TestConstants.java
@@ -2,34 +2,5 @@ package supercell.api.wrapper.essentials;
 
 public class TestConstants {
 
-    public static final String CHANGELOG_CONSISTENT_FILENAME =
-            "./src/test/resources/CHANGELOG_consistent.md";
-    public static final String CHANGELOG_INCONSISTENT_TITLE =
-            "./src/test/resources/CHANGELOG_inconsistent_title.md";
-    public static final String CHANGELOG_INCONSISTENT_HEAD =
-            "./src/test/resources/CHANGELOG_inconsistent_head.md";
-    public static final String CHANGELOG_INCONSISTENT_CURRENT =
-            "./src/test/resources/CHANGELOG_inconsistent_current.md";
     public static final String EMPTY = "";
-    public static final String FILE = "./src/test/resources/file.txt";
-    public static final String NOT_EXISTING_FILE = "xxx.txt";
-    public static final String NULL_STRING = null;
-    public static final String PARAMETER = "xyz";
-    public static final String README_CONSISTENT_FILENAME =
-            "./src/test/resources/README_consistent.md";
-    public static final String README_INCONSISTENT_GRADLE_NAME =
-            "./src/test/resources/README_inconsistent_gradle_name.md";
-    public static final String README_INCONSISTENT_GRADLE_VERSION =
-            "./src/test/resources/README_inconsistent_gradle_version.md";
-    public static final String README_INCONSISTENT_TITLE_NAME =
-            "./src/test/resources/README_inconsistent_title_name.md";
-    public static final String README_INCONSISTENT_TITLE_VERSION =
-            "./src/test/resources/README_inconsistent_title_version.md";
-    public static final String README_INCONSISTENT_MAVEN_VERSION =
-            "./src/test/resources/README_inconsistent_maven_version.md";
-    public static final String README_TITLE = "olala";
-    public static final String TEST_POM_FILENAME = "./src/test/resources/test_pom.xml";
-    public static final String VERSION_100_FILENAME = "./src/test/resources/version_100.txt";
-    public static final String VERSION_DIFFERENT_THAN_TEST_POM_FILENAME =
-            "./src/test/resources/version_different.txt";
 }
